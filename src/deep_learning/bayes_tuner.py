aac27e7e610afdba215757eb9cacbb3f0447ff76
EC2 Default User <ec2-user@ip-172-31-66-132.ec2.internal>
2025-04-12 06:15:31 +0000

    updated bayes_tuner.py

--- src/deep_learning/bayes_tuner.py
+++ src/deep_learning/bayes_tuner.py
@@ -36,6 +36,11 @@ class BayesTune(FullFT):
         )
         self.method_name = 'Bayes_FT'
         self.sparsity_level = sparsity_level
+        self.trainable_params = []
+        self.trainable_param_names = []
+        self.initial_params = {}
+        self.param_shapes = []
+        self.lambda_stats = None
 
     def unfreeze_selected_layers(self):
         # Freeze all layers
@@ -43,22 +48,38 @@ class BayesTune(FullFT):
             param.requires_grad = False
         
         # Unfreeze layornorm layers
-        selected_layers = []
         for name, module in self.model.named_modules():
-            if isinstance(module, torch.nn.LayerNorm):
+            # LayerNorm
+            #if isinstance(module, torch.nn.LayerNorm):
+            #    for param_name, param in module.named_parameters():
+            #        param.requires_grad = True
+            #        selected_layers.append((f"{name}.{param_name}", param))
+            # LayerNorm + project layer in attention
+
+            # Target specific module names
+            if ("layer_norm" in name )or ("self_attn" in name and "proj" in name) or ('linear' in name) :#or ('fc' in name):
                 for param_name, param in module.named_parameters():
                     param.requires_grad = True
-                    selected_layers.append((f"{name}.{param_name}", param))
-            
+                    # Recover full parameter name
+                    full_param_name = f"{name}.{param_name}" if name else param_name
+
+                    self.trainable_params.append(param)
+                    self.trainable_param_names.append(full_param_name)
+                    self.initial_params[full_param_name] = param.detach().clone()
+                    self.param_shapes.append(list(param.shape))
+            #    module.requires_grad = True
+            #    selected_layers.append((name, module))
+
         # Count total parameters and linear parameters
-        total_params = sum(p.numel() for p in self.model.parameters())
-        selected_params = sum(p.numel() for _, p in selected_layers)
+        count_total_params = sum(p.numel() for p in self.model.parameters())
+        count_trainable_params = sum(p.numel() for p in self.trainable_params)
         
-        print(f"Enabled {len(selected_layers)} parameter groups with {selected_params} parameters")
-        print(f"Total model parameters: {total_params}")
-        print(f"Percentage of parameters enabled: {selected_params/total_params*100:.2f}%")
         
-        return selected_layers
+        print(f"Enabled {len(self.trainable_params)} parameter groups with {count_trainable_params} parameters")
+        print(f"Total model parameters: {count_total_params}")
+        print(f"Percentage of parameters enabled: {count_trainable_params/count_total_params*100:.2f}%")
+        
+        return count_trainable_params
 
 
     def get_posterior_stats(
@@ -66,8 +87,8 @@ class BayesTune(FullFT):
         alpha=0.01,
         beta=100,
         lambda_init=0.0001,
-        lr_model=2e-5,
-        lr_lambda=0.01,
+        lr_model=0.0005,
+        lr_lambda=0.0005,
         num_epochs=1,
         N_exp=12,
         noise_discount=1.0,
@@ -84,20 +105,20 @@ class BayesTune(FullFT):
         self.model = self.model.to(self.device)
 
         # Parameter shapes and names
-        self.param_shapes = []
-        self.trainable_param_names = []
+        #self.param_shapes = []
+        #self.trainable_param_names = []
 
         # Filter trainable parameters
-        for name, param in self.model.named_parameters():
-            if param.requires_grad:
-                self.param_shapes.append(list(param.shape))
-                self.trainable_param_names.append(name)
+        #for name, param in self.model.named_parameters():
+        #    if param.requires_grad:
+        #        self.param_shapes.append(list(param.shape))
+        #        self.trainable_param_names.append(name)
 
         # Create a copy of initial parameters
-        self.initial_params = {}
-        for name, param in self.model.named_parameters():
-            if param.requires_grad:
-                self.initial_params[name] = param.detach().clone()
+        #self.initial_params = {}
+        #for name, param in self.model.named_parameters():
+        #    if param.requires_grad:
+        #        self.initial_params[name] = param.detach().clone()
 
         # Initialize lambda parameters only for trainable parameters
         self.lambda_params = nn.ParameterList([
@@ -128,6 +149,8 @@ class BayesTune(FullFT):
         # SGLD updates both model parameters and regularization parameter lambda, 
         # but this is purely for sampling purposes, not for training
         # 
+
+        print("Sampling with SGLD...")
         for epoch in range(num_epochs):
             self.model.train()
             
@@ -212,6 +235,7 @@ class BayesTune(FullFT):
 
             # Log lambda statistics at the end of the epoch
             with torch.no_grad():
+                print('Sampling completed;')
                 print(f"Lambda stats - Mean: {lambda_stats['running_mean'].mean().item():.6f}, "
                       f"Min: {lambda_stats['running_mean'].min().item():.6f}, "
                       f"Max: {lambda_stats['running_mean'].max().item():.6f}, "
@@ -221,17 +245,22 @@ class BayesTune(FullFT):
         if hasattr(torch.cuda, 'empty_cache'):
             torch.cuda.empty_cache()
         gc.collect()
-
+        self.lambda_stats = lambda_stats
+    
         return lambda_stats
         
     def Tune(self):
         # For testing purpose, only target the linear layers
 
         # Set all the linear layers to be trainable
-        self.unfreeze_selected_layers()
+        count_trainable_params = self.unfreeze_selected_layers()
+
 
         posterior_stats = self.get_posterior_stats()
         lambda_mean = posterior_stats['running_mean']
+
+        assert lambda_mean.shape[0] == count_trainable_params, "Lambda mean size doesn't match trainable parameters count"
+
         print('Lambda Mean Example: ', lambda_mean[:5])
 
         # If the above works, use that lambda_mean to select which parameters to set requires_grad = True.
@@ -239,42 +268,38 @@ class BayesTune(FullFT):
         # Sort lambda values and keep top parameters based on sparsity level
         sorted_values, sorted_indices = lambda_mean.sort()
         threshold_idx = int(len(lambda_mean) * self.sparsity_level)
-        update_indices = sorted_indices[-threshold_idx:]
+        #update_indices = sorted_indices[-threshold_idx:]
+        lambda_thres = sorted_values[-threshold_idx].item()
         
         print(f"Sparsity level {self.sparsity_level} (updating {threshold_idx} of {len(lambda_mean)} parameters)")
-        print(f"Lambda threshold: {sorted_values[-threshold_idx].item():.6f}")
+        print(f"Lambda threshold: {lambda_thres:.6f}")
 
         # Track global index
-        global_idx = 0
-        updatable_params = []
+        p_idx = 0
         total_selected = 0
         
         # Selectively make it trainable
-        for name, param in self.model.named_parameters():
-            param_size = param.numel()
+        for name in self.trainable_param_names:
+            params = self.model.get_parameter(name)
+            param_size = params.numel() # the number of parameters 
             
-            # Check if any of the indices in the current parameter are in the update_indices
-            local_indices = []
-            for i in range(param_size):
-                if global_idx + i in update_indices:
-                    local_indices.append(i)
-            
-            # If any indices are found, set the chunk of parameters to be trainable            
-            if local_indices:
-                param.requires_grad = True
-                updatable_params.append(param)
-                print(f"Parameter {name}: {len(local_indices)}/{param_size} elements updatable ({len(local_indices)/param_size*100:.4f}%)")
-                total_selected += len(local_indices)
-            # Increment global index
-            global_idx += param_size
+            param_lambda = lambda_mean[p_idx:p_idx + param_size]
+            importance_mask = param_lambda >= lambda_thres
+            importance_ratio = importance_mask.float().mean().item()
+            if importance_ratio > 0.05: # Only select parameters with more than 5% importance
+                # Still trying to see good threshold here, 0.01 is too small, 0.1 is too big.
+                params.requires_grad = True
+                total_selected += param_size
+                print(f"Parameter {name}: {param_size} elements updatable ({total_selected/count_trainable_params*100:.4f}%)")
 
-            if total_selected >= threshold_idx:
-                break
+            # Increment global index
+            p_idx += param_size
+
+            #if total_selected >= threshold_idx:
+            #    break
         
         # Show total updatable parameters
-        total_params = sum(p.numel() for p in self.model.parameters())
-        updatable_count = sum(p.numel() for p in updatable_params)
-        print(f"Total updatable parameters: {updatable_count}/{total_params} ({updatable_count/total_params*100:.4f}%)")
+        print(f"Total updatable parameters: {total_selected/count_trainable_params} ({total_selected/count_trainable_params*100:.4f}%)")
         
         model_save_path = f"bayes_tune_{self.model_name}.pth"
 
