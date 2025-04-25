ec3415f21b83f433e6a38c1ba656b8c632777a53
Katsuya Masaki <130842054+BenBooby@users.noreply.github.com>
2025-04-25 08:03:18 -0700

    Updated for reproducibility (#8)

--- src/main.py
+++ src/main.py
@@ -42,7 +42,9 @@ if __name__ == "__main__":
     np.random.seed(cfg.seed)
     torch.manual_seed(cfg.seed)
     torch.cuda.manual_seed_all(cfg.seed)
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = True # Added this
+    torch.backends.cudnn.benchmark = False  # Change this to False
+    torch.use_deterministic_algorithms(True, warn_only=False)  # Added this
     torch.set_float32_matmul_precision("high")
 
     # sets model if necessary
